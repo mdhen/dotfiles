1be2958cfb5938b439aed6865b16ea20109b9ffa
mdhen <matt@hennigan.dev>
2021-10-02 18:06:14 +0100

    add fish config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+theme_gruvbox
+
+# gnome keyring pass password on
+if test -n "$DESKTOP_SESSION"
+    set -x (gnome-keyring-daemon --start | string split "=")
+end
+
+set -x JAVA_HOME '/usr/lib/jvm/java-8-openjdk'
+
+set -x ANDROID_HOME '/opt/android-sdk'
+set -x ANDROID_SDK '/opt/android-sdk'
